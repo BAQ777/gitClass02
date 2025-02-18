efceeaf2d37e25c66d467db98104abcc6179a1ee
BAQ777 <baquiroz777@gmail.com>
2025-02-17 20:02:41 -0500

    person2commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -2,8 +2,8 @@ public class main {
 
     public static void main(String[] args) {
 
-        System.out.println("this is the project version 4");
+        System.out.println("this is the project version 5 made by person 2);
 
     }
 }
-// done with version 4
+// done with version 5
